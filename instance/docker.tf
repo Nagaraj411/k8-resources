db044552ac059ddf545219294e0c1658de500cd3
Nagaraj <nagarajuande255@gmail.com>
2025-07-23 13:25:42 +0530

    K8

--- instance/docker.tf
+++ instance/docker.tf
@@ -1,21 +1,21 @@
 # This script will run instance & security Group inbound rules & ourbound rules 
-resource "aws_instance" "docker" {
+resource "aws_instance" "K8" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.medium" # Instance type, can be changed as needed
-  vpc_security_group_ids = [aws_security_group.allow_all_docker.id] # Security Group ID
+  vpc_security_group_ids = [aws_security_group.allow_all_K8.id] # Security Group ID
 
   root_block_device {
     volume_size = 50
     volume_type = "gp3" # or "gp2", depending on your preference
   }
 
-    user_data = file("docker.sh") # User data script to install Docker
+    user_data = file("docker.sh") # User data script to install K8
   tags = { # Tags for the instance
-    Name = "Docker" # Name of the instance
+    Name = "K8" # Name of the instance
   }
 }
-resource "aws_security_group" "allow_all_docker" {
-    name        = "allow_all_docker"
+resource "aws_security_group" "allow_all_K8" {
+    name        = "allow_all_K8"
     description = "allow all traffic"
 
     ingress {
@@ -34,6 +34,6 @@ resource "aws_security_group" "allow_all_docker" {
     }
 
     tags = {
-        Name = "allow-all-docker"
+        Name = "allow-all-K8"
     }
 }
